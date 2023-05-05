5206609533ff4f8d8d27cf3aef90ab89016b4518
cseazeem <89725100+cseazeem@users.noreply.github.com>
2023-05-05 14:22:58 +0530

    Delete PairAlreadyExistsException.java

--- src/main/java/com/driver/PairAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-public class PairAlreadyExistsException extends RuntimeException {
-    public PairAlreadyExistsException(String movie, String director){
-        super("Pair of Movie " + movie + " and Director " + director + " already exists in database");
-    }
-}
