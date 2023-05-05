6e48832e989fe709bd6af6d16d2444b0c688d8c4
cseazeem <89725100+cseazeem@users.noreply.github.com>
2023-05-05 14:22:28 +0530

    Delete MovieAlreadyExistsException.java

--- src/main/java/com/driver/MovieAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-public class MovieAlreadyExistsException extends RuntimeException {
-    public MovieAlreadyExistsException(String name){
-        super("Movie " + name + " already exists in database");
-    }
-}
