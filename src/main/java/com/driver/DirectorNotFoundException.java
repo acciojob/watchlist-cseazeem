1413c9b305f204564519789110d365410b56d6fa
cseazeem <89725100+cseazeem@users.noreply.github.com>
2023-05-05 14:22:12 +0530

    Delete DirectorNotFoundException.java

--- src/main/java/com/driver/DirectorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-public class DirectorNotFoundException extends RuntimeException {
-    public DirectorNotFoundException(String name){
-        super("Director not found");
-    }
-}
