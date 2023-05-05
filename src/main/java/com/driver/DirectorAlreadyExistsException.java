2b38b05fd5e52abb837c758f7574215b16ed74fc
cseazeem <89725100+cseazeem@users.noreply.github.com>
2023-05-05 14:22:00 +0530

    Delete DirectorAlreadyExistsException.java

--- src/main/java/com/driver/DirectorAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-public class DirectorAlreadyExistsException extends RuntimeException {
-    public DirectorAlreadyExistsException(String name){
-        super("Director " + name + " already exists in database");
-    }
-}
